2a6a040e96e04c6f5844a985c2a60390a517474d
Ivan Sigaev <ivan.develop@gmail.com>
2025-02-23 22:22:16 +0100

    feat: update product endpoint

--- bin/aws-cloud-fe.ts
+++ bin/aws-cloud-fe.ts
@@ -5,7 +5,7 @@ import { NodejsAwsShopReactStack } from '../lib/nodejs-aws-shop-react-stack';
 const app = new cdk.App();
 new NodejsAwsShopReactStack(app, 'NodejsAwsShopReactStack', {
   env: {
-    region: 'us-east-1',
     account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 });
