bc4cdc72e295f12e59dce1c33342b1d4339b97f3
Ivan Sigaev <ivan.develop@gmail.com>
2025-02-23 22:41:07 +0100

    fix: endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://1aozz31sqa.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://1aozz31sqa.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://1aozz31sqa.execute-api.eu-central-1.amazonaws.com/prod",
+  bff: "https://1aozz31sqa.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "https://1aozz31sqa.execute-api.eu-central-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
