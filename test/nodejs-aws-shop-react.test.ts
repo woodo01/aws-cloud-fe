6b9c5f59e68035bf56d5ef0afa300fafff24ba76
Ivan Sigaev <ivan.develop@gmail.com>
2025-02-23 22:15:39 +0100

    feat: add react app

---
+++ test/nodejs-aws-shop-react.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NodejsAwsShopReact from '../lib/nodejs-aws-shop-react-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nodejs-aws-shop-react-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NodejsAwsShopReact.NodejsAwsShopReactStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
