40a31983669db6bc0c344c356fde576ceaa2aec6
Ivan Sigaev <ivan.develop@gmail.com>
2025-04-06 13:26:16 +0200

    feat(task8): add db implementation

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://pzt3kbw4edoxwxojig7abawwtm0vcpqi.lambda-url.eu-central-1.on.aws/api/profile/cart",
   import: "https://d8oebp8k99.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://vbwskwtvqb.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://vbwskwtvqb.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "https://pzt3kbw4edoxwxojig7abawwtm0vcpqi.lambda-url.eu-central-1.on.aws/api",
 };
 
 export default API_PATHS;
