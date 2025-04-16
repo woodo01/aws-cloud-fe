cec5694740b80b6711ec1ba0e5a6939b154d4c6b
Ivan Sigaev <ivan.develop@gmail.com>
2025-04-16 17:35:59 +0200

    feat(task9): update order/cart endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://vbwskwtvqb.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "http://woodo01-cart-api-dev.eu-central-1.elasticbeanstalk.com/api/profile/cart",
+  order: "https://d1zjgok5y3qwpo.cloudfront.net/api/profile/cart",
   import: "https://d8oebp8k99.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://vbwskwtvqb.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "http://woodo01-cart-api-dev.eu-central-1.elasticbeanstalk.com/api",
+  cart: "https://d1zjgok5y3qwpo.cloudfront.net/api",
 };
 
 export default API_PATHS;
